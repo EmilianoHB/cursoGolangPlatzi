77800ca2d47ae45b8859f93431cd13bdaf4d4d2d
EmilianoHB <EmilianoHBlanco@gmail.com>
2023-02-07 05:01:51 -0300

    Clase 4, tipos de datos primitivos

--- src/main.go
+++ src/main.go
@@ -3,55 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	x := 10
-	y := 50
+	/*
+		Numeros enteros
+		int = Depende del OS (32 o 64 bits)
+		int8 = 8bits = -128 a 127
+		int16 = 16 bits
+		int32 = 32 bits
+		int64 = 64 bits
 
-	// Suma
-	result := x + y
-	fmt.Println("Suma: ", result)
+		Numeros enteros positivos
+		unit = Depende del OS (32 o 64 bits)
+		unit8 = 8bits
+		unit16 = 16 bits
+		unit32 = 32 bits
+		unit64 = 64 bits
 
-	//Resta
-	result = y - x
-	fmt.Println("Resta:", result)
+		Numeros decimales
+		float32 = 32 bits
+		float64 = 64 bits
 
-	//Multiplicacion
-	result = x * y
-	fmt.Println("Multiplicacion: ", result)
+		Textos y booleanos
+		string = ""
+		bool = true o false
 
-	//Division
-	result = y / x
-	fmt.Println("Division: ", result)
-
-	//Modulo
-	result = y % x
-	fmt.Println("Modulo: ", result)
-
-	//Incremental
-	x++
-	fmt.Println("Incremental", x)
-
-	//Decremental
-	x--
-	fmt.Println("Decremental:", x)
-
-	//Retos
-	//Calcular el area de un rectangulo, trapecio y de un circulo
-	//Area rectangulo
-	rLargo := 6
-	rAncho := 2
-	AreaRectangulo := rLargo * rAncho
-	fmt.Println("El area del rectangulo es: ", AreaRectangulo)
-
-	//Area Trapecio
-	base1 := 10
-	base2 := 4
-	alturaT := 4
-	AreaTrapecio := ((base1 + base2) * alturaT) / 2
-	fmt.Println("El area del trapecio es: ", AreaTrapecio)
-
-	//Area Circulo
-	radio := 0.5
-	const pi = 3.14
-	AreaCirculo := pi * (radio * radio)
-	fmt.Println("El area del circulo es: ", AreaCirculo)
+		Numeros complejos
+		Complex64 = Real e imaginario float32
+		Complex128 = Real o imaginario float64
+		Ejemplo: c := 10 +8i
+	*/
+	fmt.Println("Tipos de datos primitivos")
 }
