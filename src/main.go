4cb46aacb97ead7f32e6759431becb89d31defec
EmilianoHB <EmilianoHBlanco@gmail.com>
2023-02-06 21:12:47 -0300

    Clase de constantes, variables y zero values

--- src/main.go
+++ src/main.go
@@ -1,8 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hola mundo")
+	//Declacracion de constatntes
+	const pi float64 = 3.14
+	const pi2 = 3.1415
 
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2:", pi2)
+
+	//Derclaracion de variables enteras
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	//Zero values
+	var a int
+	var b float32
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	// Area de un cuadrado
+	areaCuadrado := base * altura / 2
+	fmt.Println(areaCuadrado)
 }
