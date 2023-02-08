ebdde5affe48d75908c245b6fb599e3e3ffae1f7
EmilianoHB <EmilianoHBlanco@gmail.com>
2023-02-08 17:54:04 -0300

    Clase de defer, break y continue

--- src/main.go
+++ src/main.go
@@ -3,21 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	switch modulo := 4 % 2; modulo {
-	case 0:
-		fmt.Println("Es par")
-	default:
-		fmt.Println("Es impar")
-	}
+	//defer
+	defer fmt.Println("Hola")
+	fmt.Println("Mundo")
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
 
-	//Sin condicion
-	value := 200
-	switch {
-	case value > 100:
-		fmt.Println("Es mayor a 100")
-	case value < 0:
-		fmt.Println("Es menor a 0")
-	default:
-		fmt.Println("No condicion")
+		//Continue
+		if i == 2 {
+			fmt.Println("Es 2")
+			continue
+		}
+
+		//break
+		if i == 8 {
+			fmt.Println("Break")
+			break
+		}
 	}
 }
