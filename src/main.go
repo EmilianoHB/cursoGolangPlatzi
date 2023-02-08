65cf0c7c4cd57062f5a057e6315cc7c63dd726d0
EmilianoHB <EmilianoHBlanco@gmail.com>
2023-02-08 00:08:11 -0300

    Clase sobre el condicional if

--- src/main.go
+++ src/main.go
@@ -1,106 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
 
+func parOimpar(a int) {
+	if a%2 == 0 {
+		par := "Es par"
+		fmt.Println(par)
+	} else {
+		impar := "Es impar"
+		fmt.Println(impar)
+	}
+}
+
+func iniciarsesion(a string, b int) {
+	if a == "Emiliano" && b == 1234 {
+		fmt.Println("Correcto")
+	} else {
+		fmt.Println("Incorrecto")
+	}
+
+}
 func main() {
-	fmt.Println("-------------For condicional-------------")
-	// For condicional
-	for i := 0; i <= 5; i++ {
-		fmt.Println(i)
+	valor1 := 1
+	valor2 := 2
+	if valor1 == 1 {
+		fmt.Println("Es 1")
+	} else {
+		fmt.Println("No es 1")
 	}
 
-	fmt.Println("-------------For while-------------")
-	// For while
-	counter := 0
-	for counter < 10 {
-		fmt.Println(counter)
-		counter++
+	//Whith and
+	if valor1 == 1 && valor2 == 2 {
+		fmt.Println("Es verdad")
 	}
 
-	fmt.Println("-------------For forever-------------")
-	//For forever
-	counterForever := 0
-	for {
-		fmt.Println(counterForever)
-		counterForever++
-		if counterForever > 12 {
-			break
-		}
+	//Whit or
+	if valor1 == 0 || valor2 == 2 {
+		fmt.Println("Se cumple la condicion")
 	}
 
-	fmt.Println("-------------For Range-------------")
-	//For Range
-	arreglo := [8]int{0, 1, 4, 6, 10, 9}
-	fmt.Println("Arreglo:", arreglo)
-
-	fmt.Println("Primer ejemplo")
-	for i, j := range arreglo {
-		fmt.Printf("indice i: %d tiene como valor #%d\n", i, j)
+	//Convertir texto a numero
+	value, err := strconv.Atoi("53")
+	if err != nil {
+		log.Fatal(err)
 	}
+	fmt.Println("Value: ", value)
 
-	fmt.Println("Segundo ejemplo")
-	for i := range arreglo {
-		fmt.Printf("Valor de i: %d\n", i)
-	}
-
-	fmt.Println("Tercer ejemplo")
-	for _, j := range arreglo {
-		fmt.Printf("Valor de i: %d\n", j)
-	}
-
-	fmt.Println("-------------For con funciones-------------")
-	// For con funciones
-	for i := preFor(); condicion(i); i = postFor(i) {
-		fmt.Printf("Valor de i: %d", i)
-		if i == 7 {
-			fmt.Printf(" así que saldremos del ciclo...\n")
-			break /// este ejemplo es para usar el break
-		}
-		fmt.Printf("\n")
-	}
-
-	fmt.Println("-------------For con goto tags-------------")
-	var i int
-CICLO:
-	fmt.Println("estamos fuera del for")
-	for i < 10 {
-		if i == 6 {
-			i = i + 3
-			fmt.Println("Saltando a etiqueta CICLO con i = i + 3")
-			goto CICLO2
-		}
-		fmt.Printf("Valor de i: %d\n", i)
-		i++
-	}
-CICLO2:
-	fmt.Printf("ciclo 2 Valor de i: %d\n", i)
-	if i == 9 {
-		fmt.Printf("Valor de i: %d\n", i)
-		i = i + 3
-		fmt.Println("Saltando a etiqueta CICLO con i = i + 3")
-		goto CICLO
-	}
-	fmt.Printf("terminamos\n")
-
-	fmt.Println("-------------Reto for decreciente-------------")
-	// Reto for decreciente
-	for i := 10; i >= 0; i-- {
-		fmt.Println(i)
-	}
-
-}
-
-func preFor() int {
-	fmt.Println("prefor i")
-	return 0
-}
-func postFor(i int) int {
-	fmt.Println("postFor sumemos i")
-	i++
-	return i
-}
-func condicion(i int) bool {
-	fmt.Println("condicion i")
-
-	return (i < 10)
+	//Retos
+	parOimpar(9)
+	iniciarsesion("Emiliano", 1234)
 }
