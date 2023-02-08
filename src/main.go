ab39e606359621fdc918da3804d86a504ecc6ed0
EmilianoHB <EmilianoHBlanco@gmail.com>
2023-02-08 18:32:42 -0300

    Clase de arrays y slices

--- src/main.go
+++ src/main.go
@@ -3,22 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	//defer
-	defer fmt.Println("Hola")
-	fmt.Println("Mundo")
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
+	//Array
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
+	/*
+	   La funcion len nos dice la cantidad de elementos encontramos dentro de la array o
+	   slice. Mientras que la funcion cap lo que hace es decirnos la capacidad
+	   maxima que tiene dicho arreglo o slice
+	*/
+	//Slice
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
 
-		//Continue
-		if i == 2 {
-			fmt.Println("Es 2")
-			continue
-		}
+	//Metodos en el slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
 
-		//break
-		if i == 8 {
-			fmt.Println("Break")
-			break
-		}
-	}
+	//Append
+	slice = append(slice, 7)
+	fmt.Println(slice)
+
+	//Append nueva lista
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice, len(slice), cap(slice))
 }
