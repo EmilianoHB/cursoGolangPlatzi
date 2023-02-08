bb3dcbd79d56dd0234956181ac3afec91e8fe175
EmilianoHB <EmilianoHBlanco@gmail.com>
2023-02-08 17:28:50 -0300

    Clase switch

--- src/main.go
+++ src/main.go
@@ -1,56 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"strconv"
-)
+import "fmt"
 
-func parOimpar(a int) {
-	if a%2 == 0 {
-		par := "Es par"
-		fmt.Println(par)
-	} else {
-		impar := "Es impar"
-		fmt.Println(impar)
-	}
-}
-
-func iniciarsesion(a string, b int) {
-	if a == "Emiliano" && b == 1234 {
-		fmt.Println("Correcto")
-	} else {
-		fmt.Println("Incorrecto")
-	}
-
-}
 func main() {
-	valor1 := 1
-	valor2 := 2
-	if valor1 == 1 {
-		fmt.Println("Es 1")
-	} else {
-		fmt.Println("No es 1")
+	switch modulo := 4 % 2; modulo {
+	case 0:
+		fmt.Println("Es par")
+	default:
+		fmt.Println("Es impar")
 	}
 
-	//Whith and
-	if valor1 == 1 && valor2 == 2 {
-		fmt.Println("Es verdad")
+	//Sin condicion
+	value := 200
+	switch {
+	case value > 100:
+		fmt.Println("Es mayor a 100")
+	case value < 0:
+		fmt.Println("Es menor a 0")
+	default:
+		fmt.Println("No condicion")
 	}
-
-	//Whit or
-	if valor1 == 0 || valor2 == 2 {
-		fmt.Println("Se cumple la condicion")
-	}
-
-	//Convertir texto a numero
-	value, err := strconv.Atoi("53")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Value: ", value)
-
-	//Retos
-	parOimpar(9)
-	iniciarsesion("Emiliano", 1234)
 }
