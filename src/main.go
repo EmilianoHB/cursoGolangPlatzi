6674479b18e5c744d01017fc957c204085a9c343
EmilianoHB <EmilianoHBlanco@gmail.com>
2023-02-09 22:37:33 -0300

    Clase de primer contacto con las Gorutines

--- src/main.go
+++ src/main.go
@@ -3,26 +3,46 @@ package main
 import (
 	pk "cursoGolangPlatzi/src/mypackage"
 	"fmt"
+	"sync"
+	"time"
 )
 
 func main() {
-	cuadradito := pk.Cuadrado{
-		Base: 3,
-	}
-	rectangulito := pk.Rectangulo{
-		Base:   2,
-		Altura: 5,
-	}
+	/* El paquete sync permite interacturar de forma primitiva
+	con las gorutine. Variable que acomula un conjunto de gorutines
+	y los va liberando poco a poco */
+	var wg sync.WaitGroup
 
-	pk.Calcular(cuadradito)
-	pk.Calcular(rectangulito)
+	fmt.Println("Hello")
 
-	//Lista de interfaces
-	miInterfaz := []interface{}{
-		"Hola",
-		12,
-		4.90,
-		true,
-	}
-	fmt.Println(miInterfaz...)
+	/* Indicamos que vamos a agregar 1 Gorutine al WaitGroup
+	para que espere su ejecucion antes de que la gurutine
+	base (main) muera, y así le de tiempo a la siguiente
+	gorutine de ejecutarse */
+	wg.Add(1)
+
+	/* La palabra reservada go ejecutara la funcion de forma
+	concurrente */
+	go pk.Say("world", &wg)
+
+	/* Funcion del WaitGroup que sirve para decirle
+	al gorutine principal (main) que espere hasta que
+	todas las gorutine del WaitGroup finalicen,
+	es decir, hasta que se ejecute 'defer wg.Done()'
+	en cada una de las goroutines*/
+	wg.Wait()
+
+	//Funcion anonima
+	func(text string) {
+		fmt.Println(text)
+	}("Adios")
+
+	/* ! Funcion para que cuando llegue a esta linea
+	espere el tiempo indicado (lo suficiente para que
+	la Gorutine ejecute su funcion de forma concurrente) */
+	time.Sleep(time.Second * 1)
+
+	/* Nota: Para fines practicos se hace uso
+	de la funcion Sleep(), pero en realidad NO es una
+	buena practica, es mejor utilizar los WaitGroups */
 }
--- src/mypackage/mypackage.go
+++ src/mypackage/mypackage.go
@@ -1,41 +1,12 @@
 package mypackage
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-// Creamos un struct de cuadrado
-type Cuadrado struct {
-	Base float64
-}
-
-// Creamos una funcion del caluclo del area del cuadrado
-func (c Cuadrado) Area() float64 {
-	return c.Base * c.Base
-}
-
-// Creamos un struct de rectangulo
-type Rectangulo struct {
-	Base   float64
-	Altura float64
-}
-
-// Creamos la funcion del calculo del cuadrado
-func (r Rectangulo) Area() float64 {
-	return r.Base * r.Altura
-}
-
-/*
-Creamos un interface
-Lo que hace unicamente es tomar la funcion que comparten los dos
-Struct
-*/
-type Figuras2D interface {
-	Area() float64
-}
-
-/*
-Funcion calcular lo que hace es tomar como entrada la interfaz
-que creamos antes y hacemos que ejecute la funcion de area
-*/
-func Calcular(f Figuras2D) {
-	fmt.Println("Area: ", f.Area())
+func Say(text string, wg *sync.WaitGroup) {
+	defer wg.Done() /* Esta linea se va a ejecutar hasta el final
+	de la funcion, y de esta forma libera el gorutine del WaitGroup*/
+	fmt.Println(text)
 }
